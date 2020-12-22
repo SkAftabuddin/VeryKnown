8b788619ca0b9a3310adf1e33e9ec3bb9e621d6e
Shaikh Aftabuddin <53236880+SkAftabuddin@users.noreply.github.com>
2020-12-22 14:44:12 +0545

    Add files via upload

---
+++ New folder/hj.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>particle effect</title>
+<style type="text/css" media="screen">
+	body{
+		margin: 0;
+		padding: 0;
+	}
+	#particles-js{
+		background-image: linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(de.jpg);
+		background-size: 100%;
+
+	}
+</style>
+</head>
+<body>
+	<div id="particles-js">
+		
+	</div>
+<script src="particles.js" type="text/javascript" ></script>
+<script src="app.js" type="text/javascript" ></script>
+</body>
+</html>
